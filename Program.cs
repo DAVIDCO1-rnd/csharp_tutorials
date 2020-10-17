a684ee2e332a81c4e9da2ad0301771193c49f5eb
David Cohn <DAVIDCO1@rnd-hub.com>
2020-10-18 00:51:45 +0300

    Changed Program.cs from delegates code example to ThreadPool code example

--- Program.cs
+++ Program.cs
@@ -1,59 +1,51 @@
 ﻿using System;
+using System.Threading;
 
-namespace SortWithDelegate
+public class Example 
 {
-  public delegate int CompareDeleg(int a, int b);
- 
-  class Program
-  {
-    static int[] arr;
- 
-    static int CompareAsc(int x, int y)
+    public static void Main( )
     {
-      return x - y;
+        for(int i=0;i<5;i++)
+        {
+            // have to wait or threadpool never gives out threads to requests
+            //Thread.Sleep(50);
+            // queue thread request
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc),i);
+        }
+        // have to wait here or the background threads in the thread
+        // pool would not run before the main thread exits.
+        Console.WriteLine("Main Thread waiting to complete...");
+        bool working = true;
+        int workerThreads = 0;
+        int completionPortThreads = 0;
+        int maxWorkerThreads = 0;
+        int maxCompletionPortThreads = 0;
+        // get max threads in the pool
+        ThreadPool.GetMaxThreads(out maxWorkerThreads,out maxCompletionPortThreads);
+        while(working)
+        {
+            // get available threads
+            ThreadPool.GetAvailableThreads(out workerThreads,out completionPortThreads);
+            if(workerThreads == maxWorkerThreads)
+            {
+                // allow to quit
+                working = false;
+            }
+            else
+            {
+                // sleep before checking again
+                Thread.Sleep(500);
+            }
+        }
+        Console.WriteLine("Main Thread completing...");
     }
- 
-    static int CompareDesc(int x, int y)
+    static int counter;
+    static void ThreadProc(Object stateInfo)
     {
-      return y - x;
+        //show we did something with this thread...
+        //Console.WriteLine("Thread {0} running...", stateInfo);
+        Thread.Sleep(100);
+        Interlocked.Increment(ref counter);
+        Console.WriteLine("Counter = {0}", counter);
     }
- 
-    static void Sort(CompareDeleg compareMethod)
-    {
-      for (int i = 0; i < arr.Length - 1; i++)
-        for (int j = i + 1; j < arr.Length; j++)
-          if (compareMethod(arr[i], arr[j]) > 0)
-            Replace(i, j);
-    }
- 
-    private static void Replace(int i, int j)
-    {
-      int tmp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = tmp;
-    }
- 
-    static void PrintArr()
-    {
-      foreach (int num in arr)
-        Console.Write(num + ",");
- 
-      Console.WriteLine();
-    }
- 
-    static void Main(string[] args)
-    {
-      arr = new int[] { 123, 200, -63, 2, 7612, -13, 8 };
-      Console.WriteLine("Orginal nuumbers:");
-      PrintArr();
- 
-      Sort(new CompareDeleg(CompareAsc));
-      Console.WriteLine("\nAscending order:");
-      PrintArr();
- 
-      Sort(new CompareDeleg(CompareDesc));
-      Console.WriteLine("\nDescending order:");
-      PrintArr();
-    }
-  }
 }
